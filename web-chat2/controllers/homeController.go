68c4d0266479cd8961961470918bd32091d69d24
medaka333 <139121621+medaka333@users.noreply.github.com>
2023-11-28 14:31:51 +0900

    Update homeController.go

--- web-chat2/controllers/homeController.go
+++ web-chat2/controllers/homeController.go
@@ -1,4 +1,3 @@
-// Masa作
 package controllers
 
 import (
@@ -32,11 +31,8 @@ func Lists(c *gin.Context) {
 		}
 	}
 	// 友達IDをもとにusersテーブルから友達の名前を取得。 IN:スライスを扱える。Pluck():スライス形式で渡せる
-	// var friends_name []string
 	var users []models.Users
-	// result = initializers.DB.Model(&models.Users{}).Where("id IN ?", friendsIDs).Pluck("user_name", &friends_name)
 	result = initializers.DB.Where("id IN ?", friendsIDs).Find(&users)
-
 	if result.Error != nil {
 		c.HTML(http.StatusOK, "home.html", gin.H{
 			"title": "エラーが発生しました",
@@ -66,7 +62,6 @@ func Lists(c *gin.Context) {
 	}
 	c.HTML(http.StatusOK, "home.html", gin.H{
 		"title": "Home",
-		// "friends_name": friends_name,
 		"users": users,
 		"rooms": rooms,
 	})
@@ -132,10 +127,6 @@ func AddFriend(c *gin.Context) {
 		Result(c, "ユーザーを追加できませんでした", models.Users{})
 		return
 	}
-	// Hubを作成
-	// h := websocket.NewHub()
-	// go h.Run()
-	// models.RoomToHub[room.ID] = h
 
 	c.Redirect(http.StatusSeeOther, "/home")
 }
@@ -241,143 +232,3 @@ func Result(c *gin.Context, title string, user models.Users) {
 		"user":  user,
 	})
 }
-
-//非同期処理しようとした
-// func loadFriendListAsync(user models.Users, ch chan []string) {
-// 	// フレンドリストをデータベースから取得する処理
-// 	var friends []models.Friends
-// 	initializers.DB.Where("user_id1 = ? OR user_id2 = ?", user.ID, user.ID).Find(&friends)
-// 	// 友達のユーザーIDのみを抽出
-// 	var friendIDs []uint
-// 	for _, friend := range friends {
-// 		if friend.UserID1 == user.ID {
-// 			friendIDs = append(friendIDs, friend.UserID2)
-// 		} else {
-// 			friendIDs = append(friendIDs, friend.UserID1)
-// 		}
-// 	}
-// 	// 友達のユーザー名を取得
-// 	var friendNames []string
-// 	initializers.DB.Model(&models.Users{}).Where("id IN ?", friendIDs).Pluck("user_name", &friendNames)
-// 	// フレンドリストをチャネルに送信
-// 	ch <- friendNames
-// }
-
-//	func Lists(c *gin.Context) {
-//		user, _ := c.Get("user")
-//		// フレンドリストのチャネルとグループリストのチャネルを作成
-//		friendListCh := make(chan []string)
-//		groupListCh := make(chan []models.Rooms)
-//		// 非同期でフレンドリストとグループリストを取得
-//		go loadFriendListAsync(user.(models.Users), friendListCh)
-//		go loadGroupListAsync(user.(models.Users), groupListCh)
-//		// チャネルからデータを受け取る
-//		friendNames := <-friendListCh
-//		rooms := <-groupListCh
-//		// テンプレートにデータを渡して表示
-//		c.HTML(http.StatusOK, "home.html", gin.H{
-//			"title":        "Home",
-//			"friends_name": friendNames,
-//			"rooms":        rooms,
-//		})
-//	}
-
-// func loadGroupListAsync(user models.Users, ch chan []models.Rooms) {
-// 	// グループリストをデータベースから取得する処理
-// 	var groups []models.Groups
-// 	initializers.DB.Where("users_refer = ?", user.ID).Find(&groups)
-// 	// グループに関連するルーム情報を取得
-// 	var roomIDs []uint
-// 	for _, group := range groups {
-// 		roomIDs = append(roomIDs, group.RoomsRefer)
-// 	}
-// 	var rooms []models.Rooms
-// 	initializers.DB.Where("id IN ?", roomIDs).Find(&rooms)
-// 	// グループリストをチャネルに送信
-// 	ch <- rooms
-// }
-
-// todoの処理
-// func TodoCreate(c *gin.Context) {
-// 	user, _ := c.Get("user")
-// 	todo := models.Todo{
-// 		UserID: user.(models.User).ID,
-// 	}
-// 	content := (c.PostForm("content"))
-// 	todo.Content = content
-// 	if content == "" {
-// 		c.JSON(http.StatusBadRequest, gin.H{
-// 			"error": "何か書いてください",
-// 		})
-// 		return
-// 	}
-// 	result := initializers.DB.Debug().Create(&todo)
-// 	if result.Error != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{
-// 			"error": "Failed to create todo",
-// 		})
-// 		return
-// 	}
-// 	c.Redirect(http.StatusMovedPermanently, "/todos/list")
-// }
-
-// func TodoUpdate(c *gin.Context) {
-// 	user, _ := c.Get("user")
-// 	todo := models.Todo{
-// 		UserID: user.(models.User).ID,
-// 	}
-// 	id, _ := strconv.Atoi(c.PostForm("id"))
-// 	// .Where().Whereで繋ぐことで’かつ'AND演算子の役割をする
-// 	initializers.DB.Debug().Where("id=?", id).Where("user_id=?", todo.UserID).Find(&todo)
-// 	// 15
-// 	// 2
-// 	// 古い内容
-// 	content := c.PostForm("content")
-// 	todo.Content = content
-// 	// todo
-// 	// gorm.Model = todoリストのID　＝15（15番目に作られたtodoリスト）
-// 	// UserID     user.ID 誰が作ったtodoかわかる=2（2番目に登録したユーザー）
-// 	// Content    =新しい内容
-// 	result := initializers.DB.Debug().Updates(&todo)
-// 	if result.Error != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{
-// 			"error": "Failed to update todo",
-// 		})
-// 		return
-// 	}
-// 	c.Redirect(http.StatusMovedPermanently, "/todos/list")
-// }
-
-// func MoveToEdit(c *gin.Context) {
-// 	user, _ := c.Get("user")
-// 	todo := models.Todo{
-// 		UserID: user.(models.User).ID,
-// 	}
-// 	id, err := strconv.Atoi(c.Query("id"))
-// 	if err != nil {
-// 		log.Fatalln(err)
-// 	}
-// 	initializers.DB.Debug().Where("id=?", id).Where("user_id=?", todo.UserID).Find(&todo)
-// 	c.HTML(http.StatusOK, "edit.html", gin.H{
-// 		"title": user.(models.User).UserName + "'s Todos",
-// 		"todo":  todo,
-// 	})
-// }
-
-//	func DeleteTodo(c *gin.Context) {
-//		user, _ := c.Get("user")
-//		id, err := strconv.Atoi(c.Query("id"))
-//		if err != nil {
-//			log.Fatalln(err)
-//		}
-//		// Unscoped:物理削除
-//		// Where("id = ? AND user_id = ?", id, user.(models.User).ID).Delete(&models.Todo{})
-//		result := initializers.DB.Debug().Unscoped().Where("id = ?", id).Where("user_id = ?", user.(models.User).ID).Delete(&models.Todo{})
-//		if result.Error != nil {
-//			c.JSON(http.StatusBadRequest, gin.H{
-//				"error": "Failed to delete todo",
-//			})
-//			return
-//		}
-//		c.Redirect(http.StatusSeeOther, "/todos/list")
-//	}
